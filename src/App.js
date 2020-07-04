493627df25312eb6f28e2213276a81628d2066df
Md. Shafayet Kabir <shafayet.me@gmail.com>
2020-07-04 06:52:53 +0600

    setState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from "react";
 import "./App.css";
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      {/* Functional components */}
-      <Greet name="shafayet" shortName="SFT">
-        <p>This is child of Shafayet</p>
-      </Greet>
-      <Greet name="kabir" shortName="KBR">
-        <button type="button" class="btn btn-primary">
-          Primary
-        </button>
-      </Greet>
-      <Greet name="shajib" shortName="SJB" />
-
-      {/* Class components */}
-      <Welcome name="shafayet" shortName="SFT">
-        <p>This is child of Shafayet</p>
-      </Welcome>
-      <Welcome name="kabir" shortName="KBR">
-        <button type="button" class="btn btn-primary">
-          Primary
-        </button>
-      </Welcome>
-      <Welcome name="shajib" shortName="SJB"></Welcome>
+      <Message />
     </div>
   );
 }
